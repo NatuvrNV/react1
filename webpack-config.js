be269e42c3edda7bc132a79a285f4981e4bdccf4
NatuvrNV <197884416+NatuvrNV@users.noreply.github.com>
2025-09-16 18:21:43 +0530

    Update webpack-config.js

--- webpack-config.js
+++ webpack-config.js
@@ -1,34 +1,63 @@
-
-
-
+const path = require("path");
 const TerserPlugin = require("terser-webpack-plugin");
-
-module.exports = {
-  optimization: {
-    minimize: true,
-    minimizer: [new TerserPlugin()],
-  },
-};
-
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { PurgeCSSPlugin } = require("purgecss-webpack-plugin");
+const glob = require("glob");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
+  mode: "production", // enables many optimizations by default
+  entry: "./src/index.js",
+  output: {
+    filename: "[name].[contenthash].js", // cache-busting
+    path: path.resolve(__dirname, "dist"),
+    clean: true, // cleans /dist folder automatically
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"], // for modern JS support
+      },
+    ],
+  },
+
+  plugins: [
+    new CleanWebpackPlugin(), // cleans old builds
+    new MiniCssExtractPlugin({
+      filename: "[name].[contenthash].css", // cache-busting CSS
+    }),
+    new PurgeCSSPlugin({
+      paths: glob.sync(`${path.join(__dirname, "src")}/**/*`, { nodir: true }),
+      safelist: () => ({
+        standard: [/^navbar/, /^modal/], // keep dynamic classes (Bootstrap/Tailwind, etc.)
+      }),
+    }),
+  ],
+
   optimization: {
     minimize: true,
     minimizer: [
-      `...`, // Keep existing minimizers (like Terser)
+      new TerserPlugin({
+        extractComments: false,
+        terserOptions: {
+          compress: {
+            drop_console: true, // remove console.log in production
+          },
+        },
+      }),
       new CssMinimizerPlugin(),
     ],
-  },
-};
-
-
-
-module.exports = {
-  optimization: {
     splitChunks: {
-      chunks: "all",
+      chunks: "all", // better caching
     },
+    runtimeChunk: "single", // separates runtime for long-term caching
   },
 };
-
