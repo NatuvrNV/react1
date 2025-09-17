2b9063cd3a7ec7150536cdac546d83c7890076ba
NatuvrNV <197884416+NatuvrNV@users.noreply.github.com>
2025-09-17 15:23:18 +0530

    Update webpack-config.js

--- webpack-config.js
+++ webpack-config.js
@@ -1,34 +1,34 @@
-
-
-
 const TerserPlugin = require("terser-webpack-plugin");
-
-module.exports = {
-  optimization: {
-    minimize: true,
-    minimizer: [new TerserPlugin()],
-  },
-};
-
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
 module.exports = {
+  mode: "production", // ensure production optimizations
   optimization: {
     minimize: true,
     minimizer: [
-      `...`, // Keep existing minimizers (like Terser)
+      new TerserPlugin({
+        extractComments: false, // don’t generate LICENSE.txt
+        terserOptions: {
+          compress: {
+            drop_console: true,   // remove console.log
+            drop_debugger: true,  // remove debugger
+          },
+        },
+      }),
       new CssMinimizerPlugin(),
     ],
-  },
-};
-
-
-
-module.exports = {
-  optimization: {
     splitChunks: {
       chunks: "all",
+      minSize: 20000, // Split bundles bigger than 20kb
+      maxSize: 250000, // Ensure bundles aren’t too huge
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: "vendors",
+          chunks: "all",
+        },
+      },
     },
+    runtimeChunk: "single", // Extract runtime for better caching
   },
 };
-
