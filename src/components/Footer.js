3c47c0eb407c5ca033bd705b46359816001e77d2
NatuvrNV <197884416+NatuvrNV@users.noreply.github.com>
2025-12-02 10:40:28 +0530

    Adjust footer layout and image sizes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,13 +9,13 @@ const Footer = () => {
   return (
     <footer className="tw-mt-14 tw-pb-8 tw-max-w-[1100px] tw-mx-auto tw-px-4">
     <div className="Desk-footer">
-    <div className="tw-flex tw-flex-col md:tw-flex-row tw-justify-center tw-items-center md:tw-items-start tw-gap-8 md:tw-gap-32 font ">
+    <div className="tw-flex tw-flex-col md:tw-flex-row tw-justify-center tw-items-start md:tw-items-start tw-gap-8 md:tw-gap-32 font ">
         {/* Image 1 */}
        <div className="tw-flex tw-items-center tw-gap-4">
   <img className="tw-w-32 md:tw-w-40" src={logo} alt="METAGUISE Logo" />
 
   <img
-    className="tw-w-[80px]"       // 👈 80px width
+    className="tw-w-[100px]"       // 👈 80px width
     src={ADpro_icon}
     alt="ADpro Logo"
   />
