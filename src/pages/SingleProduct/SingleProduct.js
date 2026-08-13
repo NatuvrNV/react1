68aab25ac248ad5eca27c37373f16c7de55de8f9
NatuVR <info@natuvr.com>
2026-08-13 18:12:51 +0530

    website fixes

--- src/pages/SingleProduct/SingleProduct.js
+++ src/pages/SingleProduct/SingleProduct.js
@@ -42,7 +42,7 @@ const SingleProduct = () => {
 
   useEffect(() => {
     let cancelled = false;
-    fetch(`${process.env.PUBLIC_URL}/data/product-index.json`)
+    fetch(`${process.env.PUBLIC_URL}/data/products-index.json`)
       .then((res) => (res.ok ? res.json() : {}))
       .then((data) => {
         if (!cancelled) setProductCopyData(data);
